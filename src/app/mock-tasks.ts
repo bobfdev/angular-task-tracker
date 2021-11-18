c7845d46e6f956026946ea9c5eeacd3f537f78b6
Bob F <bobfdev@gmail.com>
2021-11-18 13:25:06 -0600

    updated initial files and add Task.ts and mock-tasks.ts to the app folder

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Dentist Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Haircut',
+        day: 'May 8th at 1:00pm',
+        reminder: true,
+    },
+];
